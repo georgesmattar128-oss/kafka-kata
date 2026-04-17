b6ba81dd2d0e1fed65c33d360269358728f6e977
2419n <94062223+georgesmattar12@users.noreply.github.com>
2026-04-17 18:43:21 +0200

    branche step1 corrigé et fonctionnel
    branche step1 corrigé et fonctionnel

--- src/main/java/com/learn/kafka/KafkaApplication.java
+++ src/main/java/com/learn/kafka/KafkaApplication.java
@@ -1,13 +1,10 @@
 package com.learn.kafka;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.learn.kafka")
 public class KafkaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
 }
